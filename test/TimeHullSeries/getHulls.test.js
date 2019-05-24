ce5c0785270df24924d664de18570e1ed410402c
Matt Sears <mattsears18@gmail.com>
2019-05-24 09:19:13 -0400

    ensure that hullseries always iterate back to startIndex=0

--- test/TimeHullSeries/getHulls.test.js
+++ test/TimeHullSeries/getHulls.test.js
@@ -396,4 +396,34 @@ describe('TimeHullSeries.getHulls()', () => {
 
     expect(hullSeries.getHulls()).to.eql(['previously calculated hulls']);
   });
+
+  it('creates hulls until it iterates down to startIndex = 0', () => {
+    const points = [
+      { x: 100, y: 100, timestamp: 22 },
+      { x: 100, y: 100, timestamp: 100 },
+      { x: 100, y: 100, timestamp: 2000 },
+      { x: 100, y: 100, timestamp: 3000 },
+      { x: 100, y: 100, timestamp: 4000 },
+      { x: 100, y: 100, timestamp: 5000 },
+      { x: 100, y: 100, timestamp: 6000 },
+      { x: 100, y: 100, timestamp: 7000 },
+      { x: 100, y: 100, timestamp: 8000 },
+      { x: 100, y: 100, timestamp: 9000 },
+      { x: 100, y: 100, timestamp: 10000 },
+      { x: 100, y: 100, timestamp: 11000 },
+      { x: 100, y: 100, timestamp: 12000 },
+      { x: 100, y: 100, timestamp: 13000 },
+      { x: 100, y: 100, timestamp: 14000 }
+    ];
+
+    const hullSeries = new TimeHullSeries({
+      points,
+      period: 5000
+    });
+
+    console.log(hullSeries.getHulls());
+
+    expect(hullSeries.getHulls()[0].startIndex).to.equal(0);
+    expect(hullSeries.getHulls().length).to.equal(10);
+  });
 });
